d8d63b4406165d8f4be3cb004b836d6400bc8aa4
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-03-26 11:43:20 -0500

    I Do Declare! Assignment

---
+++ Web_Fundamentals/JavaScript/I Do Declare!.js
@@ -0,0 +1,28 @@
+// Declare variables of the following types:
+// 4 Numbers
+// 3 Strings
+// 2 Booleans
+// 1 Undefined Variable
+// Give the variables descriptive names (ex: string1 and string2)
+// Print each one to the console with a description (ex: console.log("The First String:", string1);)
+// Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!)
+var number1 = 28;
+var number2 = -9;
+var number3 = 2.9;
+var number4 = 1000;
+var string1 = "Coding";
+var string2 = "dojo";
+var string3 = "belt";
+var boolean1 = true;
+var boolean2 = false;
+var undefined
+console.log("number 1 is: " + number1);
+console.log("number 2 is: " + number2);
+console.log("number 3 is: " + number3);
+console.log("number 4 is: " + number4);
+console.log("string 1 is: " + string1);
+console.log("string 2 is: " + string2);
+console.log("string 3 is: " + string3);
+console.log("boolean 1 is: " + boolean1);
+console.log("boolean 2 is: " + boolean2);
+console.log("undefined is: " + undefined)
