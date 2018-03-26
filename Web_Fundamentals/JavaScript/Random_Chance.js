ba580e761f56d3b3621d3defcc80e84929504a76
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-03-26 15:10:33 -0500

    Random Chance assignment

---
+++ Web_Fundamentals/JavaScript/Random_Chance.js
@@ -0,0 +1,18 @@
+function playSlots(quarters, leaveAt) {
+    for (quarters; quarters > 0; quarters--) {
+        if (quarters >= leaveAt){
+            console.log("you are leaveing with " + quarters + " quarters");
+            return;
+        } 
+        if ((Math.trunc(Math.random()*100)+1) == 100) {
+            var winnings = Math.trunc(Math.random()*50) + 51;
+            console.log("you win " + winnings + " quarters. CONGRATS!");
+            quarters += winnings;
+            console.log("you have " + (quarters -1) + " left.")
+        } else {
+            console.log("You Lost. You have " + (quarters - 1) + " left");
+        }
+    }
+    return 0;
+}
+playSlots(300,350);
