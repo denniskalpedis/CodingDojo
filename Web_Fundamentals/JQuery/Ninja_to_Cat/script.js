c18048d8b2304f056757973faa859ffb0cc8a1fa
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-03-27 17:57:27 -0500

    Debugging jQuery assignment.

--- Web_Fundamentals/JQuery/Ninja_to_Cat/script.js
+++ Web_Fundamentals/JQuery/Ninja_to_Cat/script.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-    // $('img').click(function(){
-    //     $(this).css("visibility", "hidden");
-    // });
-    // $("#reset").click(function(){
-    //     $(this).parent().children("div").children("img").css("visibility", "visible");
-    // })
     var temp;
     $('img').click(function() {
         temp = $(this).attr('altsrc');
