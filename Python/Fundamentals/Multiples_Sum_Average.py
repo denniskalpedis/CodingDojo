776c9b1ae151590491a865a6dfbf2f2dd89afaeb
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-04-02 15:05:21 -0500

    Multiple, Sum, Average assignment.

---
+++ Python/Fundamentals/Multiples_Sum_Average.py
@@ -0,0 +1,8 @@
+for odd in range(1, 1000, 2): #range starting at 1 and going to 1000 incrimenting by 2.
+    print odd
+for multiple_of_five in range(5, 1000001, 5): #range starting at 5 and going to 1,000,000 incrementing by 5.
+    print multiple_of_five
+a = [1, 2, 5, 10, 255, 3]
+print sum(a) #sum all values in the list
+
+print sum(a)/len(a) #sum all values in the list and divide by the length
