1f758e6930da19157dfe586f8db9c332b2ff2e0f
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-04-03 13:31:41 -0500

    Scores and Grades assignment.

---
+++ Python/Fundamentals/Scores_and_Grades.py
@@ -0,0 +1,13 @@
+import random
+print "Scores and Grades"
+for _1 in range(0,10):
+    score = random.randint(60,100)
+    if score >= 90:
+        grade = "A"
+    elif score >= 80:
+        grade = "B"
+    elif score >=70:
+        grade = "C"
+    else:
+        grade = "D"
+    print "Score: {}; Your grade is {}".format(score, grade)
