c4c0e3b35b6fe9c589fd34c4899291cc7c5cc23e
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-04-03 08:46:26 -0500

    some comments.

--- Python/Fundamentals/Finding_characters.py
+++ Python/Fundamentals/Finding_characters.py
@@ -1,4 +1,10 @@
 word_list = ['hello','world','my','name','is','Anna']
 char = 'o'
 matching = [s for s in word_list if char in s]
+'''
+matching = []
+for s in word_list:
+    if char in s:
+        matching.append(s)
+'''
 print matching
