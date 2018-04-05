8618aadb92fbe9df1e284c17d8315a3e7d5c026e
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-04-05 17:43:47 -0500

    What's my name? assignment.

--- Python/Flask_Fundamentals/Hello_World/hello_world.py
+++ Python/Flask_Fundamentals/Hello_World/hello_world.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-app.run(debug=True)din
+
+app.run(debug=True)
---
+++ Python/Flask_Fundamentals/Whats_my_name/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def create_user():
+    print "Got Post Info"
+    # recall the name attributes we added to our form inputs
+    # to access the data that the user input into the fields we use request.form['name_of_input']
+    name = request.form['name']
+    print name
+    return redirect('/')
+
+app.run(debug=True)
