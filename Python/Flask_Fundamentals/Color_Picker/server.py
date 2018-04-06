192b2a68d97085d437db203fa9f1e2b782b560f6
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-04-06 17:33:47 -0500

    Color Picker assignment.

---
+++ Python/Flask_Fundamentals/Color_Picker/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    
+    return render_template('/index.html')
+
+@app.route('/change', methods=['POST'])
+def get_info():
+    r = request.form['red']
+    g = request.form['green']
+    b = request.form['blue']
+    color = "rgb(" + r + ", " + g + ", " + b + ")"
+    
+    return render_template('/index.html', color = color, r = r, g = g, b = b)
+
+app.run(debug=True)
