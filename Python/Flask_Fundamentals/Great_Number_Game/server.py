f80db9592717a541dd1f37ff26607481aa108c24
denniskalpedis <dennis.kalpedis@gmail.com>
2018-04-07 22:58:28 -0500

    Change to colors in the Great Number Game assignment.

--- Python/Flask_Fundamentals/Great_Number_Game/server.py
+++ Python/Flask_Fundamentals/Great_Number_Game/server.py
@@ -7,11 +7,10 @@ app.secret_key='Deadpool'
 def index():
     if 'secret_number' not in session:
         session['secret_number'] = random.randrange(0,101)
-        print session['secret_number']
-    color = 'red'
+    bg_color = [0, 255]
     display = 'none'
     winner = False
-    return render_template('index.html', winner = winner, display = display, color = color)
+    return render_template('index.html', r = bg_color[0], g = bg_color[1], winner = winner, display = display)
 
 
 @app.route('/', methods=['POST'])
@@ -19,7 +18,7 @@ def index_post():
     if request.form['submit'] == 'Play again!':
         session['secret_number'] = random.randrange(0,101)
         winner = False
-        color = 'green'
+        bg_color = [0, 255]
         number = ''
         guess = ''
         display = 'none'
@@ -29,18 +28,34 @@ def index_post():
         winner = False
         if int(number_guess) == session['secret_number']:
             winner = True
-            color = 'green'
+            bg_color = [0, 255]
             number = str(number_guess) + ' was the'
             guess = "number!"
             session.pop('secret_number')
         elif int(number_guess) < session['secret_number']:
-            color = 'red'
+            bg_color = background_color(session['secret_number'], int(number_guess))
             number = str(number_guess) + ' is'
             guess = "too low!"
         else:
-            color = 'red'
+            bg_color = background_color(int(number_guess), session['secret_number'])
             number = str(number_guess) + ' is'
             guess = "too high!"
-    
-    return render_template('index.html', winner = winner, display = display, color = color, number = number, guess = guess)
+
+    return render_template('index.html', r = bg_color[0], g = bg_color[1], winner = winner, display = display, number = number, guess = guess)
+
+def background_color(large_num, small_num):
+    r = 0
+    g = 255
+    if (large_num - small_num <= 25):
+        g = 255
+        r += int((large_num - small_num) * 10.64)        
+    elif (large_num - small_num > 25):
+        r = 255
+        g -= int(((large_num - small_num) - 25) * 10.64)
+    if ( g < 0):
+        g = 0
+    if ( r > 255):
+        r = 255
+    return r, g
+
 app.run(debug=True)
