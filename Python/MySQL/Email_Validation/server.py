b3b37f8ff8aaa7736b9edfba83c4fe56fe61cc1a
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-04-11 21:53:49 -0500

    Full Friends assignment.

---
+++ Python/MySQL/Email_Validation/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, request, session, flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+app = Flask(__name__)
+app.secret_key = "ThisIsSecret!"
+@app.route('/', methods=['GET'])
+def index():
+  return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def submit():
+    if len(request.form['email']) < 1:
+        flash("Email cannot be blank!")
+        return redirect('/')
+    elif not EMAIL_REGEX.match(request.form['email']):
+        flash("Invalid Email Address!")
+        return redirect('/')
+    else:
+        return redirect('/success')
+app.run(debug=True)
+
+@app.route('/success')
+def
