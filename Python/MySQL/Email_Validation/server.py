125b38aacf743778e73a9c24dff7d7f50bec03b1
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-04-12 14:47:38 -0500

    Email Validation with DB assignment.

--- Python/MySQL/Email_Validation/server.py
+++ Python/MySQL/Email_Validation/server.py
@@ -1,23 +1,45 @@
 from flask import Flask, render_template, redirect, request, session, flash
 import re
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from mysqlconnection import MySQLConnector
 app = Flask(__name__)
-app.secret_key = "ThisIsSecret!"
+mysql = MySQLConnector(app,'emails')
+app.secret_key='Deadpool'
+
+
 @app.route('/', methods=['GET'])
 def index():
-  return render_template("index.html")
+  return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
 def submit():
+    EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
     if len(request.form['email']) < 1:
         flash("Email cannot be blank!")
-        return redirect('/')
     elif not EMAIL_REGEX.match(request.form['email']):
         flash("Invalid Email Address!")
-        return redirect('/')
     else:
+        query = "INSERT INTO emails (email, created_at, updated_at) VALUES (:email, NOW(), NOW())"
+        data = {
+            'email': request.form['email']
+            }
+        mysql.query_db(query, data)
+        flash("The E-Mail {} was successfully added to the database!".format(request.form["email"]))
         return redirect('/success')
-app.run(debug=True)
+    return redirect('/')
+
 
 @app.route('/success')
-def
+def success():
+    query = "SELECT * FROM emails"
+    emails = mysql.query_db(query)
+    return render_template('emails.html', emails = emails)
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    print request.form['id']
+    query = "DELETE FROM emails WHERE id = :id"
+    data = {'id': request.form['id']}
+    mysql.query_db(query, data)
+    return redirect('/success')
+
+app.run(debug=True)
