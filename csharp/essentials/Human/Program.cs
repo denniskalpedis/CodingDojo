805338b1ce902fc2b975b2a47463ecadafaf7e42
denniskalpedis <dennis.kalpedis@gmail.com>
2018-05-02 00:41:01 -0500

    Wizard, Ninja, Samurai additions to Humans assignment.

--- csharp/essentials/Human/Program.cs
+++ csharp/essentials/Human/Program.cs
@@ -7,13 +7,17 @@ namespace Human
         public class Human
         {
             public string name;
-            public int strength = 3;
-            public int intelligence = 3;
-            public int dexterity = 3;
-            public int health = 100;
+            public int strength { get; set; }
+            public int intelligence { get; set; }
+            public int dexterity { get; set; }
+            public int health { get; set; }
             public Human(string name)
             {
                 this.name = name;
+                this.strength = 3;
+                this.intelligence = 3;
+                this.dexterity = 3;
+                this.health = 100;
             }
             public Human(string name, int strength, int intelligence, int dexterity, int health)
             {
@@ -26,6 +30,55 @@ namespace Human
             public void attack(Human attacked)
             {
                 attacked.health -= this.strength*5;
+            }
+        }
+        public class Wizard : Human
+        {
+            public Wizard(string name) : base(name, 3,25,3,50)
+            {
+
+            }
+            public void heal()
+            {
+                health += 10*intelligence; 
+            }
+            public void fireball(Human attacked)
+            {
+                Random rand = new Random();
+                attacked.health -= rand.Next(20,50);
+            }
+        }
+        public class Ninja : Human
+        {
+            public Ninja(string name) : base(name, 3,3,175,100)
+            {
+
+            }
+            public void steal()
+            {
+                health += 10; 
+            }
+            public void get_away(Human attacked)
+            {
+                health -= 15;
+            }
+        }
+        public class Samurai : Human
+        {
+            public Samurai(string name) : base(name, 3,3,3,200)
+            {
+
+            }
+            public void death_blow(Human attacked)
+            {
+                if (attacked.health < 50)
+                {
+                    attacked.health = 0;
+                } 
+            }
+            public void meditate()
+            {
+                health = 200;
             }
         }
         static void Main(string[] args)
