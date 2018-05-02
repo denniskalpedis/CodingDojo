21eb65d1b0f546cbdd2d56bd8a27ee78be68800f
denniskalpedis <dennis.kalpedis@gmail.com>
2018-05-01 23:26:12 -0500

    Boxing/Unboxing assignment.

---
+++ csharp/essentials/BoxingUnboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            List<object> listofobjects = new List<object>();
+            listofobjects.Add(7);
+            listofobjects.Add(28);
+            listofobjects.Add(-1);
+            listofobjects.Add(true);
+            listofobjects.Add("chair");
+            foreach(var item in listofobjects)
+            {
+                System.Console.WriteLine(item);
+                try{
+                    total += (int)item;
+                }
+                catch (Exception ex)
+                {}
+            }
+            System.Console.WriteLine(total);
+        }
+    }
+}
