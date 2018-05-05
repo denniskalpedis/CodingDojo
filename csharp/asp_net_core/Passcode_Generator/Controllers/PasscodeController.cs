231d98e67f4e84a54eb9e3c4c28cecd280a7e4cb
denniskalpedis <dennis.kalpedis@gmail.com>
2018-05-05 01:38:44 -0500

    Random Passcode assignment.

---
+++ csharp/asp_net_core/Passcode_Generator/Controllers/PasscodeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+ 
+namespace Passcode.Controllers
+{
+    public class PasscodeController : Controller
+    {
+        Random rand = new Random();
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            int? count = HttpContext.Session.GetInt32("Count");
+            if(count == null)
+            {
+                count = 1;
+            }
+            else
+            {
+                count++;
+            }
+            HttpContext.Session.SetInt32("Count", (int)count);
+            const string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789";
+            char[] chars = new char[14];
+
+            for (int i = 0; i < 14; i++)
+            {
+                chars[i] = allowedChars[rand.Next(0, allowedChars.Length)];
+            }
+            ViewBag.passcode = new string(chars);
+            ViewBag.count = count;
+            return View();
+        }
+    }
+}
