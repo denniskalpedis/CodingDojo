f1224bd6ed9a2af642f791259059083c88aff0bc
denniskalpedis <dennis.kalpedis@gmail.com>
2018-06-10 23:52:09 -0500

    survey form with sockets.

--- MEAN/Socket/Survey_Form/server.js
+++ MEAN/Socket/Survey_Form/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
-var session = require('express-session');
 var bodyParser = require('body-parser');
+const server = app.listen(8000, function () {});
 const io = require('socket.io')(server);
 app.use(express.static(__dirname + "/static"));
 app.set('views', __dirname + '/views');
@@ -12,35 +12,33 @@ app.use(bodyParser.urlencoded({
 
 io.on('connection', function (socket) {
 
-    socket.emit('greeting', {
-        msg: 'Greetings, from server Node, brought to you by Sockets! -Server'
-    });
-    socket.on('thankyou', function (data) {
-        console.log(data.msg);
+    // socket.emit('greeting', {
+        // msg: 'Greetings, from server Node, brought to you by Sockets! -Server'
+    // });
+    socket.on('posting_form', function (data) {
+        socket.emit('updated_message', {msg: 
+            "You emitted the following information to the server:{name: '"
+            +data.name+"', location: '"+data.location+
+            "', language: '"+ data.language + "', comment: '"
+            + data.comment + "'}"});
+        socket.emit('random_number', random_number());
     });
 
 });
+function random_number(){
+    return "Your lucky number emitted by the server is " + (Math.floor(Math.random() * (1000)) + 1);
+}
 
-app.use(session({
-    secret: 'deadpoollovesdeath',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 60000
-    }
-}));
 app.get("/", function (req, response) {
 
     response.render('index');
 });
 app.post("/submit", function (req, response) {
     console.log(req.body);
-    req.session.info = req.body;
     response.redirect('/result');
 });
 app.get("/result", function (req, response) {
     response.render('results', {
-        info: req.session.info
+        test:"test"
     });
 });
-app.listen(8000, function () {});
---
+++ MEAN/Socket/Survey_Form/static/script.js
@@ -0,0 +1,30 @@
+$(document). ready(function (){
+    
+    var socket = io(); //1
+
+    // socket.on('greeting', function (data) { //4
+        // console.log(data.msg); //5
+        // socket.emit('thankyou', { msg: 'Thank you for connecting me! -Client' }); //6
+    // });
+    socket.on('updated_message', function(data) {
+        $('#form-data').html(data.msg);
+    });
+    socket.on('random_number', function(data){
+        $('#random').html(data)
+    })
+    $('#form1').submit(function(e)
+    {
+        e.preventDefault();
+        socket.emit('posting_form', {name: $('#name').val(),
+                                    location: $('#location').val(),
+                                    language: $('#language').val(),
+                                    comment: $('#comment').val()});
+        $('#name').val('');
+        $('#location')[0].selectedIndex = 0;
+        $('#language')[0].selectedIndex = 0;
+        $('#comment').val('');
+    });
+    // $("#form1").submit(function(e){
+    //     e.preventDefault();
+    // });
+});
