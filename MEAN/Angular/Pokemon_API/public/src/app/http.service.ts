e74c32dee864740510c3ce91ff04377380dcfad0
Dennis Kalpedis <dennis.kalpedis@gmail.com>
2018-06-15 17:34:07 -0500

    Restful tasks continued assignment.

--- MEAN/Angular/Pokemon_API/public/src/app/http.service.ts
+++ MEAN/Angular/Pokemon_API/public/src/app/http.service.ts
@@ -10,12 +10,12 @@ export class HttpService {
     this.getPokemon();
   }
   getPokemon(){
-    let tempObservable = this._http.get('https://pokeapi.co/api/v2/pokemon/150/');
+    let tempObservable = this._http.get('https://pokeapi.co/api/v2/pokemon/151/');
     tempObservable.subscribe(data => {console.log("Got the Pokemon!", data)
       for(var ability of data['abilities']){
         console.log (data['name'] + " ability : " + ability['ability']['name']);
         let abilityCount = this._http.get(ability['ability']['url']);
-        abilityCount.subscribe(data => console.log(data['pokemon'].length + " have the ability " + data['name'] + "!"));
+        abilityCount.subscribe(data => console.log(data['pokemon'].length + " pokemon have the ability " + data['name'] + "!"));
       }
     });
   }
